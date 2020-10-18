8b2f78ff8bb654a1d1ce004d69415d4e334a22e4
Denys Chornyi <chornijj14@gmail.com>
2020-10-18 12:03:48 +0200

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <h1>I will find a job in this month</h1>
+
+      {/* AddItemForm */}
+
+      {/* List */}
+
+      {/* Total */}
     </div>
   );
 }
