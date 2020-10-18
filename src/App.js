e837fd032839e3e4962bb5d9f7e256d28c88c620
Denys Chornyi <chornijj14@gmail.com>
2020-10-18 12:40:17 +0200

    AddItmeForm done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, {useState} from 'react';
 import './App.css';
 
+import { FormControl, Input, IconButton } from '@material-ui/core';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+
 function App() {
 
+  const [list, setList] = useState([{id: 1, title: 'Find a job', count: 1, done: false }, 
+                                    {id: 2, title: 'Bananas', count: 2, done: false }]);
+  const [input, setInput] = useState('');
+
   return (
-    <div className="App">
-      <h1>I will find a job in this month</h1>
+    <div className="app">
 
       {/* AddItemForm */}
+
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input className="app__input"
+                  placeholder='Enter a message...' 
+                  value={input}
+                  onChange={e => setInput(e.target.value)}/>
+          <IconButton className="app__iconButton"
+                type="submit" 
+                variant="contained" 
+                color="primary"
+                disabled={!input}>
+            <AddCircleOutlineIcon/>
+          </IconButton>
+        </FormControl>
+      </form>
 
       {/* List */}
 
